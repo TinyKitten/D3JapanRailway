8bb23cfd353e68230de20dba4f4abebfb1ad06d6
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-04-26 09:51:34 +0900

    stationapiでプロット

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,70 @@
 import * as topojson from 'topojson-client';
-import { select as d3Select, json as d3Json } from 'd3';
-import { geoMercator, geoPath } from 'd3-geo';
+import { select as d3Select, json as d3Json, Selection } from 'd3';
+import { geoCircle, geoMercator, geoPath } from 'd3-geo';
 import { GeoJsonProperties, Geometry, FeatureCollection } from 'geojson';
 import japanGeoPath from './geo/japan.topojson';
-
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import Station from './models/Station';
 
 const MAP_WIDTH = window.innerWidth;
 const MAP_HEIGHT = window.innerHeight;
 const MAP_SCALE = 1200;
 
+const ALL_STATIONS = gql`
+  query GetAllStations {
+    allStations {
+      name
+      latitude
+      longitude
+    }
+  }
+`;
+
+const projection = geoMercator()
+  .center([136.0, 35.6])
+  .translate([MAP_WIDTH / 2, MAP_HEIGHT / 2])
+  .scale(MAP_SCALE);
+const path = geoPath(projection).projection(projection);
+
 const App: React.FC = () => {
+  const { loading, error, data } = useQuery(ALL_STATIONS);
+  const [rootGroup, setRootGroup] = useState<
+    Selection<SVGGElement, unknown, null, undefined>
+  >();
+
+  useEffect(() => {
+    if (error) {
+      alert(error.message);
+    }
+  }, [error]);
+
   const svgRef = useRef<SVGSVGElement>(null);
+  useEffect(() => {
+    if (!rootGroup) {
+      return;
+    }
+    data?.allStations.forEach((s: Station) => {
+      const circle = geoCircle()
+        .center([s.longitude, s.latitude])
+        .radius(0.025);
+      rootGroup
+        .append('path')
+        .datum(circle)
+        .attr('class', 'circle')
+        .attr('d', path)
+        .attr('fill', 'red');
+    });
+  }, [data, rootGroup]);
+
   const createMap = useCallback(async () => {
-    const projection = geoMercator()
-      .center([136.0, 35.6])
-      .translate([MAP_WIDTH / 2, MAP_HEIGHT / 2])
-      .scale(MAP_SCALE);
-
-    const path = geoPath(projection).projection(projection);
-
     const svg = d3Select(svgRef.current)
       .attr('width', MAP_WIDTH)
       .attr('height', MAP_HEIGHT);
     const topo = await d3Json<
       TopoJSON.Topology<TopoJSON.Objects<GeoJsonProperties>>
     >(japanGeoPath);
+
     if (!topo) {
       return;
     }
@@ -33,15 +72,16 @@ const App: React.FC = () => {
       topo,
       topo.objects.japan
     ) as FeatureCollection<Geometry, GeoJsonProperties>;
-    svg
-      .append('g')
-      .selectAll('path')
+    const g = svg.append('g');
+    g.selectAll('path')
       .data(geojson.features)
       .enter()
       .append('path')
       .attr('d', path)
-      .attr('stroke', 'black')
-      .attr('fill', 'none');
+      .attr('stroke', 'white')
+      .attr('fill', 'black');
+
+    setRootGroup(g);
   }, []);
 
   useEffect(() => {
---
+++ src/models/Station.ts
@@ -0,0 +1,16 @@
+type Station = {
+  id: number;
+  address: string;
+  distance: number;
+  latitude: number;
+  longitude: number;
+  openYmd: string;
+  postalCode: string;
+  prefId: number;
+  groupId: number;
+  name: string;
+  nameK: string;
+  nameR: string;
+};
+
+export default Station;
